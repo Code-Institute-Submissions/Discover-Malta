9c3c4a9ec34f1d2b76b3a80d587e8748ce2be30e
HollyC7 <hollycaulfield1@gmail.com>
2021-01-24 11:15:48 +0000

    add comments to file

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,17 +1,19 @@
 // Some of the below code has been taken directly from the documentation for Google Places API. I have made some changes for it to suit my requirements.
 
-function initMap(){
-    const map = new google.map.Map(document.getElementById("map"), { 
+function initMap(){ 
+    const map = new google.maps.Map(document.getElementById("map"), { // Set the map centered on the Maltese Archipelago
         center: { lat: 35.9440174, lng: 14.3795242 },
         zoom: 10,
     });
 
+    // Set the search box to use autocomplete and restrict the results to Malta only. "MT" is the entire archipelago
     const input = document.getElementById("pac-input");
     const autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.setComponentRestrictions({ 
       country:["mt"]
   });
 
+    // Set the information found and returned in the info window that pops up once a place is searched for 
     autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
     const infowindow = new google.maps.InfoWindow();
     const infowindowContent = document.getElementById("infowindow-content");
@@ -20,21 +22,24 @@ function initMap(){
         map,
         anchorPoint: new google.maps.Point(0, -29),
     });
+
     autocomplete.addListener("place_changed", () => {
         infowindow.close();
         marker.setVisible(false);
         const place = autocomplete.getPlace();
 
+        // if the place searched does not exist the below is returned 
         if (!place.geometry) {
             window.alert("No details available for input: '" + place.name + "'");
         return;
         };
 
+        //if place searched exists show on the map 
         if (place.geometry.viewport) {
         map.fitBounds(place.geometry.viewport);
         } else {
         map.setCenter(place.geometry.location);
-        map.setZoom(17); // Why 17? Because it looks good.
+        map.setZoom(17); 
         }
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
