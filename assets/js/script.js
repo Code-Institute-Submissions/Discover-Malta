d08e96cda9879a6ee49225d2c2cc7adb4874109f
HollyC7 <hollycaulfield1@gmail.com>
2021-01-24 14:51:38 +0000

    add function to hide/show content to trip recommendations

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Content is hidden when page is loaded, when button is pressed the content is shown
+
+
+document.addEventListener('DOMContentLoaded', toggleContent());
+document.addEventListener('DOMContentLoaded', hideContent());
+
+
+function hideContent(){
+    $('.card-body').children('p').hide();
+};
+
+function toggleContent() {
+    $(".card-button").click(function(){
+        $(this).siblings('p').slideToggle('slow');
+    });
+};
