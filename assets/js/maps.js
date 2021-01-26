e4106778b9f85bf6065c48ef937337feaea546c0
HollyC7 <hollycaulfield1@gmail.com>
2021-01-26 15:31:58 +0000

    add map markers and marker clusters

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
-// document.addEventListener('DOMContentLoaded', initMap());
+document.addEventListener('DOMContentLoaded', MarkerClusterer());
 
 // Some of the below code has been taken directly from the documentation for Google Places API. I have made some changes for it to suit my requirements.
 
@@ -13,13 +13,15 @@ function initMap(){
 
     // Creating the sutocomplete search and info widnows displayed 
     // Set the search box to use autocomplete and restrict the results to Malta only. "MT" is the entire archipelago
+     
     const input = document.getElementById("pac-input");
     const autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.setComponentRestrictions({ 
       country:["mt"]
-    });
+    }); 
 
     // Set the information found and returned in the info window that pops up once a place is searched for 
+    
     autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
     const infowindow = new google.maps.InfoWindow();
     const infowindowContent = document.getElementById("infowindow-content");
@@ -70,29 +72,35 @@ function initMap(){
         infowindow.open(map, marker);
     });    
 
-    // Creating marker clusters for recommended places
+    // Creating marker clusters for recommended places, followed along with the course videos & API documentation
     // Array of characters to be used as the map labels
-    //const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  // Add markers to the map
-  /*
-    const markers = locations.map((location, i) => {
+    // Locations shown are the places from the trip recommendations section
+    var locations = [
+        { lat: 35.898907, lng: 14.514553 }, // Valletta
+        { lat: 35.884430, lng: 14.403160 }, // Mdina
+        { lat: 35.913760, lng: 14.489910 }, // Saint Julians
+        { lat: 35.841240, lng: 14.545070 }, // Marsalokk
+        { lat: 36.012936, lng: 14.334650 }, // Comino
+        { lat: 36.043547, lng: 14.251807 }, // Gozo
+        { lat: 35.897676, lng: 14.512664 }, // St. John's Co Cathedral
+        { lat: 35.891736, lng: 14.518429 }, // Fort St. Angelo
+        { lat: 36.014029, lng: 14.323465 }, // Blue Lagoon
+        { lat: 35.894595, lng: 14.512602 }, // Saluting Battery
+        { lat: 35.960818, lng: 14.341395 }, // Popeye Village
+        { lat: 35.827626, lng: 14.442296 }, // Ħaġar Qim
+    ];
+
+    // Adding markers
+    var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
-        position: location,
-        label: labels[i % labels.length],
+            position: location,
+            label: labels[i % labels.length],
         });
     });
 
-    // Add a marker clusterer to manage the markers.
-    new MarkerClusterer(map, markers, {
-        imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    }); */
-};
+    // Clustering Markers
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
-// Create the locations and content shown
-/*const locations = [
-  { 
-      place_id = "ChIJxUeGHShFDhMRF8lC4o7TWnQ"
-   },
-];*/
+};
