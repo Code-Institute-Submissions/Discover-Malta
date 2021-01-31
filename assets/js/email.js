7740f95b39e2763a02d77ce4eca54f6338663078
HollyC7 <hollycaulfield1@gmail.com>
2021-01-31 11:57:24 +0000

    add comments to code

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,3 +1,4 @@
+// Sending email from the contact form
 function sendMail(contactForm) {
     emailjs.send("service_hgfgk8a", "discover-malta", {
         "from_name": contactForm.firstname.value,
@@ -9,14 +10,18 @@ function sendMail(contactForm) {
         "message": contactForm.message.value
     })
     .then(
+        // Alert sent if email successful
         function(response) {
             alert("Your email has been sent, we will be in touch as soon as possible");
+            // Page reloads after user clicks "ok" on alert box
+            // Found on Stack OVerflow, linked in README
             window.location.reload(true);
         },
+        // Alert sent if email failed
         function(error) {
             alert("Your email was not sent, please try again");
             window.location.reload(true);
         }
     );
-    return false;  // To block from loading a new page
+    return false;  // stops page reloading straight away
 }
